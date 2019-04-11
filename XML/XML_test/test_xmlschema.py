6bf1b95bc86fa6fe615714b7a0952c75b8d5683c
Gabriel Torrandella <torrandellag@gmail.com>
2019-04-11 15:10:01 -0300

    Fixed the test

--- XML/XML_test/test_xmlschema.py
+++ XML/XML_test/test_xmlschema.py
@@ -13,19 +13,19 @@ class TestXML(unittest.case.TestCase):
     def test_Invalid_Schema(self):
         path = os.getcwd()+'/XML/XML_test/'
         with pytest.raises(ParseError) as excinfo:
-            xmlschema.XMLSchema("BancoInvalidSchema.xsd")
+            xmlschema.XMLSchema(path+"BancoInvalidSchema.xsd")
         
         print("ParseError: %s" % excinfo.value)
 
     def test_is_Valid_XML(self):
         path = os.getcwd()+'/XML/XML_test/'
-        XSFormacion = xmlschema.XMLSchema("BancoSchema.xsd")
-        assert XSFormacion.is_valid("Banco.xml")
+        XSFormacion = xmlschema.XMLSchema(path+"BancoSchema.xsd")
+        assert XSFormacion.is_valid(path+"Banco.xml")
 
     def test_is_Invalid_XML(self):
         path = os.getcwd()+'/XML/XML_test/'
-        XSFormacion = xmlschema.XMLSchema("BancoSchema.xsd")
-        assert not XSFormacion.is_valid("Banco_Invalido.xml")
+        XSFormacion = xmlschema.XMLSchema(path+"BancoSchema.xsd")
+        assert not XSFormacion.is_valid(path+"Banco_Invalido.xml")
     
 
 if __name__ == "__main__":
