1a09940f745e1de7a39861790a72bd9469fe1b72
Gabriel Torrandella <torrandellag@gmail.com>
2019-04-11 15:00:16 -0300

    Added test_xmlschema

--- XML/XML_test/test_xmlschema.py
+++ XML/XML_test/test_xmlschema.py
@@ -4,21 +4,30 @@ Created on Mar 19, 2019
 @author: Gabriel Torrandella
 '''
 import pytest
+import unittest
+import os
 import xmlschema
 from xmlschema.etree import ParseError
 
-class TestXML(object):
+class TestXML(unittest.case.TestCase):
     def test_Invalid_Schema(self):
+        path = os.getcwd()+'/XML/XML_test/'
         with pytest.raises(ParseError) as excinfo:
-            xmlschema.XMLSchema("formacion_invalido.xsd")
+            xmlschema.XMLSchema("BancoInvalidSchema.xsd")
         
         print("ParseError: %s" % excinfo.value)
 
     def test_is_Valid_XML(self):
+        path = os.getcwd()+'/XML/XML_test/'
         XSFormacion = xmlschema.XMLSchema("BancoSchema.xsd")
         assert XSFormacion.is_valid("Banco.xml")
 
     def test_is_Invalid_XML(self):
+        path = os.getcwd()+'/XML/XML_test/'
         XSFormacion = xmlschema.XMLSchema("BancoSchema.xsd")
         assert not XSFormacion.is_valid("Banco_Invalido.xml")
     
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
