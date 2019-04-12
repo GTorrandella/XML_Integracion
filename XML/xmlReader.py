36c94ffeff7efb4bfc49ccf8158e0a37b19c72d9
GTorrandella <torrandellag@gmail.com>
2019-04-12 02:14:46 -0300

    Update xmlReader.py

--- XML/xmlReader.py
+++ XML/xmlReader.py
@@ -9,16 +9,6 @@ documentTree = ET.parse("Banco.xml")
 root = documentTree.getroot()
 print(root.tag, root.attrib)
 
-"""
-for child in root:
-    print(child.tag, child.attrib, child.text)
-    for c2 in child:
-        print(c2.tag, c2.attrib, c2.text)        
-        for c3 in c2:
-            print(c3.tag, c3.attrib, c3.text)        
-            for c4 in c3:
-                print(c4.tag, c4.attrib, c4.text)
-"""
 for i in root.iter('caja_ahorro'):
     print(i.tag, i.attrib)
 for i in root.iter('cuenta_corriente'):
