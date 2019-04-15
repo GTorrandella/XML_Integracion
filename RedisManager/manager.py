eb62cf54f0720e93df73fce33b9fc4ec11bcd9ac
Gabriel Torrandella <torrandellag@gmail.com>
2019-04-14 23:44:54 -0300

    Now does not break if you look up an inexistent client

--- RedisManager/manager.py
+++ RedisManager/manager.py
@@ -57,12 +57,13 @@ class Manager():
     def cuentasPorTitular(self, titular):
         try:
             lista = []
-            idCliente = self._db.hget("clientes", titular).decode()
-            for idCuenta in self._db.smembers("cliente-cuenta:"+idCliente):
-                aux = [idCuenta.decode()]
-                for dato in self._db.hmget("cuenta:"+ idCuenta.decode(), "balance", "tipo", "interes"):
-                    aux.append(dato.decode())
-                lista.append(aux)
+            idCliente = self._db.hget("clientes", titular)
+            if not idCliente == None:
+                for idCuenta in self._db.smembers("cliente-cuenta:"+idCliente.decode()):
+                    aux = [idCuenta.decode()]
+                    for dato in self._db.hmget("cuenta:"+ idCuenta.decode(), "balance", "tipo", "interes"):
+                        aux.append(dato.decode())
+                    lista.append(aux)
             return lista
         except redis.exceptions.ConnectionError:
             print("Base de datos offline. Revisar la conexión.")
@@ -70,7 +71,9 @@ class Manager():
     
     def balance(self, cuenta):
         try:
-            return self._db.hget("cuenta:"+cuenta, "balance").decode()
+            bal = self._db.hget("cuenta:"+cuenta, "balance")
+            if not bal == None:
+                return bal.decode()
         except redis.exceptions.ConnectionError:
             print("Base de datos offline. Revisar la conexión.")
             return "ERROR"
