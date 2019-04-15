aef0c78b182a7fab17c92462120b3661ece688ad
Gabriel Torrandella <torrandellag@gmail.com>
2019-04-14 23:04:01 -0300

    Finished buscadorBalance

--- XML/buscadorBalance.py
+++ XML/buscadorBalance.py
@@ -11,10 +11,22 @@ class BuscadorBalance(object):
     '''
 
     def __init__(self):
-        manager = Manager()
+        self.manager = Manager()
         
-    def mostrarMenu(self):
-        pass
+    def buscar(self):
+        print(" ---Ingrese id(s) de cuenta(s) a consultar---")
+        i = input(" --> ")
+        print("==================")
+        print("------------------")
+        print(" ID |  Balance     ")
+        for i in i.split():
+            print("------------------")
+            bal = self.manager.balance(i)
+            print(self._agregarBlanco(i, 4)+"|"+self._agregarBlanco(bal, 14))
+        print("==================")
         
-    def menu(self):
-        pass
+    def _agregarBlanco(self, palabra, longitud):
+        palabra = " " + palabra + " "
+        while len(palabra) < longitud:
+            palabra += " "
+        return palabra
